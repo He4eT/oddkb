33e7d592cb074ff6184cb26ec47a3ee644aac31f
Slava <visla.vvi@gmail.com>
2023-11-28 15:54:02 +0300

    vial: init vial.json

--- keymap/config.h
+++ keymap/config.h
@@ -31,6 +31,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Vial features
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
+// looks like this DYNAMIC_MACRO_SIZE doesn't change anything
 #define DYNAMIC_MACRO_SIZE 0
-#define VIAL_TAP_DANCE_ENTRIES 0
-#define VIAL_COMBO_ENTRIES 0
+/* 8 entries take only 6 bytes more than 2 entries
+   Thus, 8 and 0 are the only reasonable values. */ 
+#define VIAL_KEY_OVERRIDE_ENTRIES 8
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
