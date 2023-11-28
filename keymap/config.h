3cb362ce6c94c01704afc5b07e3c5e281eaa24a1
Slava <visla.vvi@gmail.com>
2023-11-28 15:19:27 +0300

    keymap: port to vial-qmk

--- keymap/config.h
+++ keymap/config.h
@@ -16,9 +16,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-
 #define USE_SERIAL
 
 #define MASTER_LEFT
 #define SPLIT_USB_DETECT
 #define SPLIT_USB_TIMEOUT 2500
+
+
+#define VIAL_KEYBOARD_UID {0x8A, 0x86, 0x65, 0x9D, 0x13, 0xE1, 0xCB, 0x81}
+
+// Vial unlock
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+// Vial features
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define DYNAMIC_MACRO_SIZE 0
+#define VIAL_TAP_DANCE_ENTRIES 0
+#define VIAL_COMBO_ENTRIES 0
