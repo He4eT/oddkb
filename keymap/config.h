0e32c765d6ba2e36130714a50ba3b8e16e7df563
He4eT <He4eT@oddsquat.org>
2023-12-04 01:23:12 +0100

    keymap: set unused params to 0

--- keymap/config.h
+++ keymap/config.h
@@ -13,7 +13,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #pragma once
 
 #define USE_SERIAL
@@ -22,19 +21,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPLIT_USB_DETECT
 #define SPLIT_USB_TIMEOUT 2500
 
-
 #define VIAL_KEYBOARD_UID {0x8A, 0x86, 0x65, 0x9D, 0x13, 0xE1, 0xCB, 0x81}
 
-// Vial unlock
+/* Vial unlock */
+
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
 
-// Vial features
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
-// looks like this DYNAMIC_MACRO_SIZE doesn't change anything
+/* Vial features */
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
 #define DYNAMIC_MACRO_SIZE 0
-/* 8 entries take only 6 bytes more than 2 entries
-   Thus, 8 and 0 are the only reasonable values. */ 
-#define VIAL_KEY_OVERRIDE_ENTRIES 8
-#define VIAL_TAP_DANCE_ENTRIES 8
-#define VIAL_COMBO_ENTRIES 8
+#define VIAL_KEY_OVERRIDE_ENTRIES 0
+#define VIAL_TAP_DANCE_ENTRIES 0
+#define VIAL_COMBO_ENTRIES 0
